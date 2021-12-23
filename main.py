ba7e9cbf3e498637444fb6791a99618e334729cb
forzamilaaan <l.de.vita@student.vu.nl>
2021-12-23 15:39:37 +0100

    Updated the api with a patch method so existing video ids could be updated

--- main.py
+++ main.py
@@ -1,43 +1,82 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+
+db.create_all()
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the the video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
-videos = {}
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the the video is required", )
+video_update_args.add_argument("views", type=int, help="Views of the video is required", )
+video_update_args.add_argument("likes", type=int, help="Likes on the video is required", )
 
-def abort_nonexisting_ids(video_id):
-    if video_id not in videos:
-        abort(404, message= "Could not find video...")
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
-def abort_existing_ids(video_id):
-    if video_id in videos:
-        abort(409, message= "There is already an existing video with that id...")
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_nonexisting_ids(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find a video with that id")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_existing_ids(video_id)
-        args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201 #send response that this video id was created
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id taken...")
+
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201  # send response that this video id was created
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video does not exist, so cannot be updated")
+
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+
+        db.session.commit()
+
+        return result
 
     def delete(self, video_id):
-        abort_nonexisting_ids(video_id)
         del videos[video_id]
         return '', 204
 
@@ -47,6 +86,3 @@ api.add_resource(Video, "/video/<int:video_id>")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run(debug=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
